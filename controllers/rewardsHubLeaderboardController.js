9c42e2b31adc83051a5a3f960f9a4dbb9b8635d0
Eltham Admin <ramonareid524@outlook.com>
2026-04-29 18:31:35 -0500

    Remove customer names from leaderboard for privacy

--- controllers/rewardsHubLeaderboardController.js
+++ controllers/rewardsHubLeaderboardController.js
@@ -4,42 +4,41 @@ const RewardsHubEntry = require("../models/RewardsHubEntry");
 const getLeaderboard = async (req, res) => {
   try {
     // 🎮 GAME PLAY LEADERBOARD
-    const gameStats = await RewardsHubGamePlay.aggregate([
-      {
-        $group: {
-          _id: "$customerEkonId",
-          customerName: { $first: "$customerName" },
-          totalGames: { $sum: 1 },
-          correctAnswers: {
-            $sum: { $cond: ["$isCorrect", 1, 0] },
-          },
-          rewardsEarned: {
-            $sum: { $cond: ["$rewardGiven", 1, 0] },
-          },
-        },
+    // 🎮 GAME PLAY LEADERBOARD
+const gameStats = await RewardsHubGamePlay.aggregate([
+  {
+    $group: {
+      _id: "$customerEkonId",
+      totalGames: { $sum: 1 },
+      correctAnswers: {
+        $sum: { $cond: ["$isCorrect", 1, 0] },
       },
-      { $sort: { correctAnswers: -1, totalGames: -1 } },
-      { $limit: 10 },
-    ]);
+      rewardsEarned: {
+        $sum: { $cond: ["$rewardGiven", 1, 0] },
+      },
+    },
+  },
+  { $sort: { correctAnswers: -1, totalGames: -1 } },
+  { $limit: 10 },
+]);
 
-    // 🎁 REWARDS HUB ENTRY LEADERBOARD
-    const entryStats = await RewardsHubEntry.aggregate([
-      {
-        $group: {
-          _id: "$customerEkonId",
-          customerName: { $first: "$customerName" },
-          totalEntries: { $sum: 1 },
-          wins: {
-            $sum: { $cond: ["$isWinner", 1, 0] },
-          },
-          rewardsGiven: {
-            $sum: { $cond: ["$rewardGiven", 1, 0] },
-          },
-        },
+// 🎁 REWARDS HUB ENTRY LEADERBOARD
+const entryStats = await RewardsHubEntry.aggregate([
+  {
+    $group: {
+      _id: "$customerEkonId",
+      totalEntries: { $sum: 1 },
+      wins: {
+        $sum: { $cond: ["$isWinner", 1, 0] },
       },
-      { $sort: { wins: -1, totalEntries: -1 } },
-      { $limit: 10 },
-    ]);
+      rewardsGiven: {
+        $sum: { $cond: ["$rewardGiven", 1, 0] },
+      },
+    },
+  },
+  { $sort: { wins: -1, totalEntries: -1 } },
+  { $limit: 10 },
+]);
 
     res.json({
       success: true,
