11ea47bd618e677aa0ada72f2a0e9a2ac21282e0
Eltham Admin <ramonareid524@outlook.com>
2026-05-15 23:06:19 -0500

    Add balance sheet accounting backend

--- routes/accountingRoutes.js
+++ routes/accountingRoutes.js
@@ -5,8 +5,10 @@ const { protect } = require("../middleware/authMiddleware");
 
 const {
   getProfitAndLoss,
+  getBalanceSheet,
 } = require("../controllers/accountingController");
 
 router.get("/profit-loss", protect, getProfitAndLoss);
+router.get("/balance-sheet", protect, getBalanceSheet);
 
 module.exports = router;
